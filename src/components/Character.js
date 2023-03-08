2fd4e38484abaffc3e5348bdc5fb53eb7331129c
Aaron Belmore <belmoreaaron9@gmail.com>
2023-03-08 12:24:31 -0500

    Added Names

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from "react";
+
+function Character({ name }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  );
+}
+
+export default Character;
